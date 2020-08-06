e4aea703d057018f51c94b90183e8d54f1963827
VictorGil <victorgilsanz@gmail.com>
2020-08-06 18:11:02 +0200

    Minor changes related to code format

--- src/main/java/net/devaction/pubnubdemo/config/ConfigurationReader.java
+++ src/main/java/net/devaction/pubnubdemo/config/ConfigurationReader.java
@@ -11,14 +11,14 @@ import net.devaction.pubnubdemo.util.JsonUnmarshaller;
  *
  * since May 2019
  */
-public class ConfigurationReader{
+public class ConfigurationReader {
     private static final Logger log = LoggerFactory.getLogger(ConfigurationReader.class);
     
     private static final String CONFIG_FILENAME = "config.json";
     
     public ApplicationConfiguration read() throws Exception{
         String jsonString = new FileReader().readFileFromClasspath(CONFIG_FILENAME);
-        
+
         ApplicationConfiguration config = JsonUnmarshaller.unmarshall(jsonString, 
                 ApplicationConfiguration.class);
         
